2341e76e565e2b02d0e3b7280b503a0bfb190866
KevLaMDev <lamarca.k@gmail.com>
2022-03-14 13:45:43 -0400

    spring security established, jpa working, object modeling completed

--- src/main/java/com/jorojala/toolshare/repositories/AppUserRepository.java
+++ src/main/java/com/jorojala/toolshare/repositories/AppUserRepository.java
@@ -2,9 +2,10 @@ package com.jorojala.toolshare.repositories;
 
 import com.jorojala.toolshare.models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AppUserRepository extends JpaRepository <AppUser , Long> {
-
+    public UserDetails findByUsername(String username);
 }
